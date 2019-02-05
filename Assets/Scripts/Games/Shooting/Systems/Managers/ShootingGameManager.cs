0ecca39dc8b871a8435bde950fba4b7f0d95dde9
euglenach <midorimushiiii@gmail.com>
2019-02-05 21:04:34 +0900

    small

--- Assets/Scripts/Games/Shooting/Systems/Managers/ShootingGameManager.cs
+++ Assets/Scripts/Games/Shooting/Systems/Managers/ShootingGameManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 namespace Games.Shooting.Systems.Managers{
     public class ShootingGameManager : MonoBehaviour{
         public bool IsEnd { get; private set; }
+        [SerializeField] private GameObject back;
         void TransitionGame(){
             
         }
