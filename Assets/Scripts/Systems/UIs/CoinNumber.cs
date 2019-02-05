fba0ed822216f359e6765873c503cc29304c2087
toshi21 <dawn.toshiki@gmail.com>
2019-02-06 04:50:09 +0900

    コイン枚数表示

---
+++ Assets/Scripts/Systems/UIs/CoinNumber.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Systems.Managers;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinNumber : MonoBehaviour {
+
+    private Text text;
+
+	// Use this for initialization
+	void Start () {
+        text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        text.text = GameManager.Instance.CoinCount.ToString();
+	}
+}
