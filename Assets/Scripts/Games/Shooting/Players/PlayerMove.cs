5f318b01cce6e1937a2150ecadfc64eb8bb6a050
feni78 <feni@unitech.jp>
2019-02-05 19:49:51 +0900

    移動を物理演算を使用した方法に変更（可動域制限は未実装）

--- Assets/Scripts/Games/Shooting/Players/PlayerMove.cs
+++ Assets/Scripts/Games/Shooting/Players/PlayerMove.cs
@@ -5,34 +5,64 @@ using UnityEngine;
 namespace Games.Shooting.Players{
     public class PlayerMove : MonoBehaviour{
 
+        public Rigidbody rb;
         GameObject Player;
 
-        private void Move(){
-            Player = GameObject.Find("Player");
-            float movex_Min=-8f;
-            float movex_Max=8f;
-            float movey_Min=-4.5f;
-            float movey_Max=-0.5f;
+        public float moveSpeed = 3.0f;
+        public float moveX = 8f;
+        public float moveY = 8f;
 
+        /*float movex_Min = -8f;
+        float movex_Max = 8f;
+        float movey_Min = -4.5f;
+        float movey_Max = -0.5f;*/
+
+
+        void Start()
+        {
+            Player = GameObject.Find("Player");
+            rb = GetComponent<Rigidbody>();
+        }
+
+        private void Move(){
             if (Input.GetKey(KeyCode.W))
             {
-                transform.Translate(0, 0.1f, 0);
+                rb.velocity=new Vector3(0, moveY, 0);
             }
-            if (Input.GetKey(KeyCode.A))
+            if (Input.GetKeyUp(KeyCode.W))
             {
-                transform.Translate(-0.1f, 0, 0);
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                transform.Translate(0, -0.1f, 0);
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                transform.Translate(0.1f, 0, 0);
+                rb.velocity = Vector3.zero;
             }
 
-            Player.transform.position = (new Vector3(Mathf.Clamp(Player.transform.position.x, movex_Min, movex_Max),
-      Mathf.Clamp(Player.transform.position.y, movey_Min, movey_Max), Player.transform.position.z));
+            if (Input.GetKey(KeyCode.A))
+            {
+                rb.velocity = new Vector3(-moveX, 0, 0);
+            }
+            if (Input.GetKeyUp(KeyCode.A))
+            {
+                rb.velocity = Vector3.zero;
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                rb.velocity = new Vector3(0, -moveY, 0);
+            }
+            if (Input.GetKeyUp(KeyCode.S))
+            {
+                rb.velocity = Vector3.zero;
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                rb.velocity = new Vector3(moveX,0, 0);
+            }
+            if (Input.GetKeyUp(KeyCode.D))
+            {
+                rb.velocity = Vector3.zero;
+            }
+
+            /*Player.transform.position = (new Vector3(Mathf.Clamp(Player.transform.position.x, movex_Min, movex_Max),
+      Mathf.Clamp(Player.transform.position.y, movey_Min, movey_Max), Player.transform.position.z));*/
 
         }
 
@@ -40,5 +70,7 @@ namespace Games.Shooting.Players{
         {
             Move();
         }
+
+
     }
 }
