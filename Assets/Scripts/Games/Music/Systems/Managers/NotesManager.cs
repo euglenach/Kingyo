4a26964bb06fb1babbc1ad4fa0f959d7b98b7f4f
toshi21 <dawn.toshiki@gmail.com>
2019-02-05 22:04:53 +0900

    音ゲー

--- Assets/Scripts/Games/Music/Systems/Managers/NotesManager.cs
+++ Assets/Scripts/Games/Music/Systems/Managers/NotesManager.cs
@@ -12,30 +12,42 @@ namespace Games.Music.Systems.Managers {
         [SerializeField]
         private int totalNotes;
 
-        private float delta;
+        private Camera camera;
+        private float delta = -3;
+        private float distance;
+        private float top;
+        private float down;
         private int notesCount;
         private bool finished = false;
-        
 
-        private void FixedUpdate()
-        {
+        private void Start(){
+            camera = Camera.main;
+            top = camera.ViewportToWorldPoint(Vector3.one).y;
+            down = camera.ViewportToWorldPoint(Vector3.zero).y;
+            distance = top - lineYPos;
+        }
+
+        private void FixedUpdate() {
             delta += Time.deltaTime;
-
-            if (!finished
-                &&delta > NotesDate.NotesStatuses[notesCount].Time - (lineYPos / (Notes.notesSpeed / 0.02)) 
-                && notesCount < totalNotes){
-
-                Instantiate(notes, new Vector3(0 + 2 * NotesDate.NotesStatuses[notesCount].Lane, 0, 0), Quaternion.identity);
-                notesCount++;
-                if (notesCount == totalNotes) finished = true;
+            if (!finished){
+                //                                                    fps
+                var time = NotesDate.NotesStatuses[notesCount].Time - 0.02 * (distance / Notes.notesSpeed);
+                if (delta > time){
+                    Instantiate(notes,
+                                new Vector3(-2.883f + 1.916f * NotesDate.NotesStatuses[notesCount].Lane, top, 0),
+                                Quaternion.identity);
+                    notesCount++;
+                    if (notesCount == totalNotes) finished = true;
+                }
             }
 
-            for (int i = 0; i < totalNotes; i++)
-            {
-                if (delta - 0.2 < NotesDate.NotesStatuses[i].Time && NotesDate.NotesStatuses[i].Time < delta + 0.2)
-                {
-                    switch (NotesDate.NotesStatuses[i].Lane)
-                    {
+        }
+
+        private void Update()
+        {
+            for (int i = 0; i < totalNotes; i++){
+                if (delta - 0.2 < NotesDate.NotesStatuses[i].Time && NotesDate.NotesStatuses[i].Time < delta + 0.2){
+                    switch (NotesDate.NotesStatuses[i].Lane){
                         case 0:
                             if (Input.GetKeyDown(KeyCode.D)) Debug.Log("hit");
                             break;
