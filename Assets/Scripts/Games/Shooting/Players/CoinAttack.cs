de404df76ff1a2db80429aa158a0e9b9bcd97420
feni78 <feni@unitech.jp>
2019-02-06 00:46:35 +0900

    コインの発射

---
+++ Assets/Scripts/Games/Shooting/Players/CoinAttack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinAttack : MonoBehaviour {
+    Rigidbody2D rb;
+    public float moveSpeed = 3.0f;
+    public float shootX = 8f;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = new Vector3(shootX, 0, 0);
+        if (transform.position.x > 10f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Sushi")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
