def618ffb9115aa258e4251806f34948ff0f915e
toshi21 <dawn.toshiki@gmail.com>
2019-02-05 12:09:49 +0900

    音ゲー

---
+++ Assets/Scripts/Games/Music/Notes/Notes.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour {
+    [SerializeField]
+    private Camera camera;
+
+    public static float notesSpeed;
+    private Vector3 screenEnd;
+
+    private void Start()
+    {
+        screenEnd = camera.ViewportToWorldPoint(new Vector3(0, 0, 0));
+    }
+
+    void FixedUpdate () {
+        transform.position += Vector3.down * notesSpeed;
+        if (transform.position.y < screenEnd.y) Destroy(gameObject);
+	}
+	
+}
---
+++ Assets/Scripts/Games/Music/Systems/Managers/NotesManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Games.Music.Systems.Managers {
+
+    public class NotesManager : MonoBehaviour {
+        [SerializeField]
+        private GameObject notes;
+        [SerializeField]
+        private float lineYPos;
+
+        private float delta;
+        
+
+        private void FixedUpdate()
+        {
+            delta += Time.deltaTime;
+            for (int i = 0; i > 100/*総ノーツ数*/; i++) {
+                if (delta == NotesDate.NotesStatuses[i].Time - (lineYPos / Notes.notesSpeed)){
+                    Instantiate(notes, new Vector3(0 + 2 * NotesDate.NotesStatuses[i].Lane, 0, 0), Quaternion.identity);
+                }
+
+                if (delta - 1 < NotesDate.NotesStatuses[i].Time && NotesDate.NotesStatuses[i].Time < delta + 1){
+                    switch (NotesDate.NotesStatuses[i].Lane)
+                    {
+                        case 0:
+                            if (Input.GetKeyDown(KeyCode.D)) ;
+                            break;
+                        case 1:
+                            if (Input.GetKeyDown(KeyCode.F)) ;
+                            break;
+                        case 2:
+                            if (Input.GetKeyDown(KeyCode.J)) ;
+                            break;
+                        case 3:
+                            if (Input.GetKeyDown(KeyCode.K)) ;
+                            break;
+                            //コインゲット
+                    }
+                }
+            }
+        }
+    }
+}
