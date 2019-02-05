cafd56ac733c54e316c7bcc4b0cfab413b4edff1
feni78 <feni@unitech.jp>
2019-02-05 15:07:11 +0900

    移動と攻撃仮実装（弾は動かない）

--- Assets/Scripts/Games/Shooting/Players/PlayerAttack.cs
+++ Assets/Scripts/Games/Shooting/Players/PlayerAttack.cs
@@ -4,8 +4,23 @@ using UnityEngine;
 
 namespace Games.Shooting.Players{
     public class PlayerAttack : MonoBehaviour{
+
+        public GameObject kobanPrefab;
+        bool attackFlag = true;
+
         private void Attack(){
 
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(kobanPrefab, transform.position, Quaternion.identity);
+            }
+        }
+
+        void Update()
+        {
+            if (attackFlag == true){
+                Attack();
+            }
         }
     }
 }
--- Assets/Scripts/Games/Shooting/Players/PlayerMove.cs
+++ Assets/Scripts/Games/Shooting/Players/PlayerMove.cs
@@ -5,8 +5,41 @@ using UnityEngine;
 
 namespace Games.Shooting.Players{
     public class PlayerMove : MonoBehaviour{
-        private void Move(){
 
+        GameObject Player;
+
+        private void Move(){
+            Player = GameObject.Find("Player");
+            float movex_Min=-8f;
+            float movex_Max=8f;
+            float movey_Min=-4.5f;
+            float movey_Max=-0.5f;
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(0, 0.1f, 0);
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(-0.1f, 0, 0);
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(0, -0.1f, 0);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(0.1f, 0, 0);
+            }
+
+            Player.transform.position = (new Vector3(Mathf.Clamp(Player.transform.position.x, movex_Min, movex_Max),
+      Mathf.Clamp(Player.transform.position.y, movey_Min, movey_Max), Player.transform.position.z));
+
+        }
+
+        private void Update()
+        {
+            Move();
         }
     }
 }
