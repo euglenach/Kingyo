5dac3938836a7865c32cc91fe6f1479474c3fa79
euglenach <midorimushiiii@gmail.com>
2019-02-06 09:01:21 +0900

    立ち絵

---
+++ Assets/Scripts/Games/Shooting/Tatie/ChangeFace.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ChangeFace : MonoBehaviour{
+    [SerializeField] private GameObject[] tatie;
+
+    public void Change(int index){
+//        bool[] b = new bool[tatie.Count()];
+//        b[index] = true;
+        foreach (var i in Enumerable.Range(0,tatie.Count())){
+            bool b = false;
+            if (i == index) b = true;
+            tatie[i].SetActive(b);
+        }
+    }
+}
